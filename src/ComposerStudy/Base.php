c865835300e0214987de00f3e3808188ec5a739f
大雄 <1119820761@qq.com>
2020-05-25 16:50:23 +0800

    composer study start

---
+++ src/ComposerStudy/Base.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Base.php
+ * Created on 2020-05-25 16:31
+ * Created by shizhixiong@pailifan.com
+ */
+
+namespace Shizhixiong\ComposerStudy;
+
+class Base
+{
+    public function index()
+    {
+        return ['name' => 'shizhixiong', 'web' => 'blog.shizhixiong.com'];
+    }
+}
---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * test.php
+ * Created on 2020-05-25 16:41
+ * Created by shizhixiong@pailifan.com
+ */
+
+require_once './vendor/autoload.php';
+
+use Shizhixiong\ComposerStudy\Base;
+
+$base = new Base();
+
+var_dump($base->index());
